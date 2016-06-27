62b0fe06ad9699b27bcf423e6eb661f369de07ce
jusch <ee>
2016-06-27 16:29:17 +0200

    Erste Ideen für die GUI

---
+++ src/main/java/trainer/App.java
@@ -0,0 +1,47 @@
+package trainer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private static App instance;
+    private Stage stage;
+
+    public App() {
+        instance = this;
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        this.stage = stage;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/StartMenuView.fxml"));
+
+        GridPane pane = loader.load();
+        Scene scene = new Scene(pane);
+
+        stage.setScene(scene);
+        stage.setTitle("Menu");
+        stage.show();
+
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public static App getInstance (){
+        return instance;
+    }
+
+    public Stage getStage(){
+        return this.stage;
+    }
+}
