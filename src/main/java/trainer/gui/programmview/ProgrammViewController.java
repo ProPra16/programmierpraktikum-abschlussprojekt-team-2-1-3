2b71b71015c5be8df54e655106df7692b076c3c5
jusch <ee>
2016-06-28 11:37:22 +0200

    startdialog erster Entwurf

--- src/main/java/trainer/gui/programmview/ProgrammViewController.java
+++ src/main/java/trainer/gui/programmview/ProgrammViewController.java
@@ -1,4 +1,4 @@
-package trainer.gui;
+package trainer.gui.programmview;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
--- src/main/java/trainer/gui/startmenu/StartMenuViewController.java
+++ src/main/java/trainer/gui/startmenu/StartMenuViewController.java
@@ -1,17 +1,23 @@
-package trainer.gui;
+package trainer.gui.startmenu;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import trainer.App;
+import trainer.gui.ProgrammViewController;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 public class StartMenuViewController {
+
+    @FXML
+    private ListView taskfield;
 
     public void startButton() throws IOException {
 
@@ -27,7 +33,34 @@ public class StartMenuViewController {
         stage.show();
 
         TextArea tempTextArea = new TextArea();
+        tempTextArea.setEditable(false);
         ProgrammViewController.setLeftTextArea(tempTextArea);
-        ProgrammViewController.disableLeftTextArea();
     }
+
+    public File[] scanForFiles() {
+
+        FilenameFilter filenameFilter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".txt");
+            }
+        };
+
+        File folder = new File("/tasks");
+        File[] listOfAllFiles = folder.listFiles(filenameFilter);
+
+        return listOfAllFiles;
+    }
+
+    public void setFileNames() {
+
+        File[] listOfFiles = scanForFiles();
+
+        File.
+
+
+
+    }
+
+
 }
